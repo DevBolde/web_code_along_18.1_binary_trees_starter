8e16267e3532bb9a7e9cb2d147c91ac314c68685
Devin Bolden <devinbolden6@gmail.com>
2024-01-29 22:02:47 -0600

    finished

--- 1_bfs.js
+++ 1_bfs.js
@@ -8,6 +8,24 @@ class node {
 
 function traverse(root) {
   // Implement
+  const q = [];
+  const output = [];
+
+  q.push(root)
+
+  while(q.length){
+    const currentNode = q.shift()
+    output.push(currentNode.value)
+
+    if(currentNode.left){
+      q.push(currentNode.left)
+    }
+    if(currentNode.right){
+      q.push(currentNode.right)
+    }
+  }
+
+  return output;
 }
 
 // Test cases:
@@ -15,6 +33,34 @@ const head = new node(6)
 head.left = new node(3)
 head.right = new node(9)
 
+/*
+
+      6
+    /  \
+  3     9
+
+PLAN: 
+
+Step 1: utilize an empty queue.
+initialize and empty output list.
+
+queue = []
+output: [6, 3, 9]
+
+Step 2: enqueue the head into the queue
+
+Step 3: While we have value inside the queue,
+then start serving the values inside the queue.
+ by serving we mean 2 steps 
+ a: remove the value/node from the queue and add it to the output.
+ b: enqueue all the children {left & right} of the current node if it has children.
+
+STep 4: repeat step three as long as we have values inside the queue.
+
+
+Step 5: once the queue is empty return the output list.
+
+*/
 console.log(traverse(head)) // 6, 3, 9
 
 // Implement a level-order breadth-first traversal for a binary tree using the provided class for a binary tree node.
